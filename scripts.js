6962a5aa330efe96f21ab4d2d2652f93c26343d0
Matheus Pimentel <149837292+matheuspimentel1@users.noreply.github.com>
2025-07-30 10:41:08 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     const currentYear = new Date().getFullYear()
-    document.getElementById('dynamicYear').textContent = currentYear + 10
+    document.getElementById('dynamicYear').textContent = currentYear
 
     document.getElementById('toggleMenu').addEventListener('click', toggleMenu)
     function toggleMenu(){
